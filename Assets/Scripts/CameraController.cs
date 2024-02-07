de05b5cda30f2b2009742ffc24cc97805b1932fd
simp886 <simonapometkova@gmail.com>
2024-02-07 23:08:52 +0200

    Downgrading project version, imported assets from first project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // This will be the script that controls the isometric camera.
+}
---
+++ Assets/Scripts/GameFlowController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class GameFlowController : MonoBehaviour
+    {
+        [SerializeField] private string _cityViewSceneName;
+        [SerializeField] private string _mainMenuSceneName;
+
+        // Might optimise into one method. Setting up initial code
+        public void StartGame() => SceneManager.LoadScene(_cityViewSceneName);
+
+        public void ExitGame() => SceneManager.LoadScene(_mainMenuSceneName);
+    }
+}
