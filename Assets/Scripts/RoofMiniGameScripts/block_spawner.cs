b987d1a81c9e53d67d83358a9611370e3c443284
Jerome <jerome.d.moutou@gmail.com>
2024-02-21 15:33:39 +0000

    Added basic block spawner script and prefabs

---
+++ Assets/Scripts/RoofMiniGameScripts/block_spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class block_spawner : MonoBehaviour
+{
+    public GameObject[] shapePrefabs;
+    //spawn position will be set when testing
+    public Vector3 spawnPosition;
+
+
+
+    //called at start() just for testing. 
+    void Start()
+    {
+        SpawnBlock();
+    }
+
+
+    public void SpawnBlock()
+    {
+        //assign index from prefabs list(set in unity inspector)
+        int shapeIndex = Random.Range(0, shapePrefabs.Length);
+        GameObject shape = shapePrefabs[shapeIndex];
+
+        Instantiate(shape, spawnPosition, Quaternion.identity);
+    }
+
+}
