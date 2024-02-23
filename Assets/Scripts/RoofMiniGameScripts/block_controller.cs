d715509a802fdddb1be45abffd8115d9d88fad5d
Jerome <jerome.d.moutou@gmail.com>
2024-02-23 16:42:41 +0000

    Fixed rotation issue allowing rotating block to move off stage

--- Assets/Scripts/RoofMiniGameScripts/block_controller.cs
+++ Assets/Scripts/RoofMiniGameScripts/block_controller.cs
@@ -82,19 +82,74 @@ public class block_controller : MonoBehaviour
         //on rotation. Will come back to address if time allows.
 
         //if 2x2 don't place offset
-        if(gameObject.name == "2x2_single(Clone)")
+        if(!CanRotate())
         {
-            transform.Rotate(0f, 0f, 90f);
+            return;
         }
         else
         {
-            //otherwise offset by 0.25
-            transform.Rotate(0f, 0f, 90f);
-            transform.position += new Vector3(-0.25f, -0.25f, 0f);
+             if(gameObject.name == "2x2_single(Clone)")
+                {
+                    transform.Rotate(0f, 0f, 90f);
+                }
+            else
+                {
+                    //otherwise offset by 0.25
+                    transform.Rotate(0f, 0f, 90f);
+                    transform.position += new Vector3(-0.25f, -0.25f, 0f);
+                }
         }
 
 
+
+
     }
+
+bool CanRotate()
+{
+    //Function to check if a block can rotate.
+
+    /*This Function checks if the block was to rotate, would it collide with any other collider
+    and if so - return false. Returns true is no collisions detected */
+
+
+    CompositeCollider2D compositeCollider = GetComponent<CompositeCollider2D>();
+
+    //debug
+    if (compositeCollider == null)
+    {
+        Debug.LogWarning("composite collider not found.");
+        return false;
+    }
+    //get center, size and rotation of collider
+    Vector2 center = compositeCollider.bounds.center;
+    Vector2 size = compositeCollider.bounds.size;
+    float rotation = compositeCollider.transform.eulerAngles.z;
+
+    //apply rotation - this may need adjusting to account for the offset of some pieces
+    rotation += 90f;
+
+    //create array to store colliding results
+    Collider2D[] results = new Collider2D[10];
+
+    //use boxcast to check for collosions - ignores own collider
+    int count = Physics2D.OverlapBoxNonAlloc(center, size, rotation, results);
+
+    //loop to check for overlapping boxes
+    for (int i = 0; i < count; i++)
+    {
+        if (results[i].gameObject != gameObject)
+        {
+            Debug.Log("Collision detected with " + results[i].name);
+            //return false if collision detected
+            return false;
+        }
+    }
+
+    //no collisions ,return true
+    return true;
+}
+
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -119,7 +174,7 @@ public class block_controller : MonoBehaviour
     }
     void OnCollisionExit2D(Collision2D collision)
     {
-        //check for collisions
+        //check for collision exits
         if (collision.gameObject.CompareTag("leftCollider"))
         {
             Debug.Log("Collision exit on left");
